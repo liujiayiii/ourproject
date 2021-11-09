80314d6878a389345a605951c9e50d3a70f31ff5
liujiayi <163895609@qq.com>
2021-11-09 15:05:38 +0800

    第二次提交

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,11 +1,11 @@
 <template>
-<el-card>
+<div>
   用户名：<input type="text" v-model="loginForm.username" placeholder="请输入用户名"/>
   <br><br>
   密码：<input type="password" v-model="loginForm.password" placeholder="请输入密码"/>
   <br><br>
   <button v-on:click="login">登录</button>
-</el-card>
+</div>
 </template>
 
 <script>
